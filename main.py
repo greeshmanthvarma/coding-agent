a8409a96505587788fe07f411e7e300709b77559
greeshmanthvarma <greeshmanth1234@gmail.com>
2025-09-23 09:50:22 -0700

    add verbose flag

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 def main():
     load_dotenv()
@@ -13,18 +15,30 @@ def main():
         print("I need a prompt")
         sys.exit(1)
 
+    verbose_flag=False
+
+    if len(sys.argv) == 3 and sys.argv[2] == "--verbose":
+        verbose_flag=True
+
+
     prompt= sys.argv[1]
+
+    messages= [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
 
     response= client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents=prompt,
+        contents=messages,
     )
 
     print(response.text)
     if response is None or response.usage_metadata is None:
         print("Response is malformed")
         return
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens : {response.usage_metadata.candidates_token_count}")
+
+    if verbose_flag:
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens : {response.usage_metadata.candidates_token_count}")
 
 main()
