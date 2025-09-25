168e08223e78c4ac4e2396f6e2fd72a4d3ba92fc
greeshmanthvarma <greeshmanth1234@gmail.com>
2025-09-24 23:52:44 -0700

    final

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from cmd import PROMPT
 import os
 import sys
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
+from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.write_file import schema_write_file
+from functions.run_python_file import schema_run_python_file
+from call_function import call_function
 
 
 def main():
@@ -22,23 +26,69 @@ def main():
 
 
     prompt= sys.argv[1]
+    system_prompt = """
+    You are a helpful AI coding agent.
 
+    When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+    - List files and directories
+    - Read the content of a file
+    - Write to a file (create or update)
+    - Run a python file with optional arguments
+
+    When the user asks about the code project - they are referring to the
+    working directory. So, you should typically start by looking at the 
+    project's files, and figuring out how to run the project and how to run its tests,
+    you'll always want to test the tests and the actual project to verify that behavior is working.
+    All paths you provide should be relative to the working directory. 
+    You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+    """
     messages= [
         types.Content(role="user", parts=[types.Part(text=prompt)])
     ]
 
-    response= client.models.generate_content(
-        model="gemini-2.0-flash-001",
-        contents=messages,
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+            schema_get_file_content,
+            schema_write_file,
+            schema_run_python_file,
+        ]
     )
 
-    print(response.text)
-    if response is None or response.usage_metadata is None:
-        print("Response is malformed")
-        return
+    config=types.GenerateContentConfig(
+        tools=[available_functions], system_instruction=system_prompt
+    )
 
-    if verbose_flag:
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens : {response.usage_metadata.candidates_token_count}")
+    max_iters = 20
 
+    for i in range(0,max_iters):
+        response= client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            config=config,
+        )
+        if response is None or response.usage_metadata is None:
+            print("Response is malformed")
+            return
+
+        if verbose_flag:
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens : {response.usage_metadata.candidates_token_count}")
+
+        if response.candidates:
+            for candidate in response.candidates:
+                if candidate is None or candidate.content is None:
+                    continue
+                messages.append(candidate.content)
+        
+        if response.function_calls:
+            for function_call_part in response.function_calls:
+                result = call_function(function_call_part, verbose_flag)
+                messages.append(result)
+        else:
+            print(response.text)
+    
+    
+    
 main()
